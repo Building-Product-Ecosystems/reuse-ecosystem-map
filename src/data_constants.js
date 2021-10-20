73dbf0d81b0dd35f087f8a7d6c1e7fce09c80df2
Bernardo Loureiro <bernardo.pacheco@gmail.com>
2021-10-20 15:26:02 -0300

    Update contact name field

--- src/data_constants.js
+++ src/data_constants.js
@@ -5,7 +5,7 @@ export const LOCATION_COL = "Region"
 export const ROLE_COL = "Primary Role"
 export const SECONDARY_ROLE_COL = "Secondary Role (if applicable)"
 export const ADDRESS_COL = "Full Address"
-export const CONTACT_COL = "Contact Name (?)"
+export const CONTACT_COL = "Contact Name"
 export const EMAIL_COL = "Email"
 export const PHONE_COL = "Phone Number"
 export const WEBSITE_COL = "Website"
@@ -17,7 +17,7 @@ export const ALL_FIELDS = [
   "Primary Role",
   "Secondary Role (if applicable)",
   "Full Address",
-  "Contact Name (?)",
+  "Contact Name",
   "Email",
   "Phone Number",
   "Website",
